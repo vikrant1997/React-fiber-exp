fd98b5856ae1108719a08770c4bd002f3bcc09c8
SINGH VIKRANT <vikrant.s@essilor.com.sg>
2020-05-18 16:17:25 +0530

    gltf loader added

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import App from "./App";
 import App2 from "./App2";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App2 />, document.getElementById("root"));
