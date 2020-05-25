291fd12ddec9c91bf3a0ee74b4f54f6aa736c6c6
SINGH VIKRANT <vikrant.s@essilor.com.sg>
2020-05-25 19:32:27 +0530

    scene added

--- src/views/Scene.js
+++ src/views/Scene.js
@@ -1,12 +1,8 @@
 import React, { useRef, useEffect } from "react";
-import Lights from "../components/Lights";
 import { useThree, useFrame, useLoader } from "react-three-fiber";
 import Room from "../components/Room";
 import { Color } from "three";
-import * as THREE from "three";
-import Bird from "../components/Bird";
 import Bird2 from "../components/Bird2";
-import url from "../assets/flamingo.glb";
 import Car from "../components/Car";
 
 function Scene() {
@@ -14,7 +10,12 @@ function Scene() {
   scene.background = new Color("lightblue");
 
   const light = useRef();
+  const light2 = useRef();
   useEffect(() => {
+    // light2.current.castShadow = true;
+    // light2.current.target.position.set(-100, 0, 150);
+    // light2.current.shadow.camera.far = 150;
+
     light.current.castShadow = true;
     light.current.shadow.camera.visible = false;
     light.current.shadow.camera.far = 500;
@@ -26,7 +27,7 @@ function Scene() {
 
     scene.add(light.current.target);
     //comment this if helper not required
-    scene.add(new THREE.CameraHelper(light.current.shadow.camera));
+    // scene.add(new THREE.CameraHelper(light.current.shadow.camera));
   }, []);
   //comment this if helper not required
   // scene.add(new THREE.CameraHelper(camera));
@@ -42,6 +43,24 @@ function Scene() {
 
         // target={Room}
       />
+      {/* <pointLight
+        ref={light2}
+        intensity={3}
+        position={[-100, 100, -150]}
+        visible={true}
+        // castShadow={true}
+
+        // target={Room}
+      /> */}
+      {/* <directionalLight
+        ref={light}
+        intensity={2}
+        position={[-300, 200, 0]}
+        visible={true}
+        // castShadow={true}
+
+        // target={Room}
+      /> */}
       <ambientLight intensity={0.5} position={[0, 200, 0]} />
 
       <React.Suspense fallback={<mesh />}>
@@ -49,10 +68,10 @@ function Scene() {
           <boxBufferGeometry attach="geometry" args={[2, 25, 25]} />
           <meshPhongMaterial attach="material" />
         </mesh>
-        <mesh position={[20, 13, -150]} receiveShadow={true} castShadow={true}>
+        {/* <mesh position={[20, 13, -150]} receiveShadow={true} castShadow={true}>
           <boxBufferGeometry attach="geometry" args={[2, 25, 25]} />
           <meshPhongMaterial attach="material" />
-        </mesh>
+        </mesh> */}
         <mesh position={[-20, 13, 150]} receiveShadow={true} castShadow={true}>
           <boxBufferGeometry attach="geometry" args={[2, 25, 25]} />
           <meshPhongMaterial attach="material" />
@@ -60,7 +79,7 @@ function Scene() {
         <Room />
         {/* <Bird position={[0, 50, 150]} speed={1} factor={0.5} url={url} /> */}
         <Bird2 position={[-150, 50, 0]} />
-        <Car position={[150, 0, 150]} />
+        <Car position={[-100, 0, -150]} />
       </React.Suspense>
     </>
     // </mesh>
