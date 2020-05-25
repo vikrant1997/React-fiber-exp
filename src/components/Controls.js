3da1307672e560083c09d9ef2ab2197a34fb5ce2
SINGH VIKRANT <vikrant.s@essilor.com.sg>
2020-05-25 19:32:16 +0530

    controls added

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,24 +1,26 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 import { extend, useThree, useFrame } from "react-three-fiber";
 
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls";
 import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
-import initialiseListeners from "../utils/listeners";
-import * as THREE from "three";
 
 extend({ OrbitControls, PointerLockControls, FirstPersonControls });
 
 function Controls({
   zoomIn,
-  moveIn,
+  moveForward,
   moveOut,
   moveTop,
   moveDown,
   moveLeft,
   moveRight,
   zoomOut,
+  // mouseLook,
+  pointerLock,
+  oPControl,
+  fPControl,
 }) {
   console.log("controls render");
 
@@ -27,21 +29,28 @@ function Controls({
     gl: { domElement },
     setDefaultCamera,
     scene,
-    // clock,
   } = useThree();
+
   const controlsRef = useRef();
-  // const firstPersonControlsRef = useRef();
-  var clock = new THREE.Clock();
+  // const pointerControlsRef = useRef();
+  const firstPersonControlsRef = useRef();
+
   useEffect(() => {
-    clock.start();
     camera.position.set(0, 10, 100);
     camera.far = 20000;
     console.log(camera.fov);
 
     setDefaultCamera(camera);
+    if (fPControl) {
+      firstPersonControlsRef.current.enabled = fPControl;
+    }
+    if (oPControl) {
+      controlsRef.current.enabled = oPControl;
+    }
+
     // controlsRef.current.target.set(0, 100, 100);
     // controlsRef.current.center = new THREE.Vector3(0, 100, 100);
-  }, []);
+  }, [fPControl, oPControl]);
 
   useFrame(() => {
     if (zoomIn) {
@@ -55,15 +64,15 @@ function Controls({
       }
     }
     if (moveLeft) {
-      // camera.position.x -= 10;
-      camera.translateX(-10);
+      camera.position.x -= 10;
+      // camera.translateX(-10);
     }
     if (moveRight) {
-      // camera.position.x += 10;
-      camera.translateX(10);
+      camera.position.x += 10;
+      // camera.translateX(10);
       // camera.translateY(10);
     }
-    if (moveIn) {
+    if (moveForward) {
       camera.position.z -= 10;
     }
     if (moveOut) {
@@ -76,47 +85,70 @@ function Controls({
     if (moveDown) {
       camera.position.y -= 10;
     }
+    if (oPControl) {
+      controlsRef.current.update();
+    }
+    if (fPControl) {
+      firstPersonControlsRef.current.update(1);
+    }
+    // if (mouseLook) {
+    //   firstPersonControlsRef.current.activeLook = false;
+    // }
+    // if (pointerLock) {
+    //   console.log("game mode entered");
+
+    //   pointerControlsRef.current.lock();
+    // }
 
     camera.updateProjectionMatrix();
-    // console.log(clock.getDelta());
-    // console.log(clock.getDelta());
-
-    // firstPersonControlsRef.current.update(clock.getDelta() * 300);
-    controlsRef.current.update();
   });
 
   // return <></>;
   return (
     <>
-      {/* <firstPersonControls
-        ref={firstPersonControlsRef}
+      {/* <pointerLockControls
+        ref={pointerControlsRef}
         args={[camera, domElement]}
-        activeLook={true}
-        // autoForward={true}
-        constrainVertical={true}
-        // heightMax={0.1}
-        lookSpeed={0.01}
-        movementSpeed={70}
-        noFly={true}
-        verticalMax={Math.PI / 2}
-        vericalMin={Math.PI / 2}
-        lookVertical={true}
       /> */}
-      <orbitControls
-        ref={controlsRef}
-        args={[camera, domElement]}
-        enableZoom={true}
-        maxZoom={100}
-        minZoom={10}
-        // enablePan={true}
-        maxDistance={1000}
-        minPolarAngle={0}
-        maxPolarAngle={Math.PI / 2}
-        zoomSpeed={0.5}
-        enableDamping={true}
-        screenSpacePanning={true}
-        keyPanSpeed={40}
-      />
+      {fPControl ? (
+        <firstPersonControls
+          ref={firstPersonControlsRef}
+          args={[camera, domElement]}
+          // activeLook={true}
+          // autoForward={true}
+          // constrainVertical={true}
+          // heightMax={0.1}
+          lookSpeed={0.001}
+          movementSpeed={1}
+          enabled={fPControl}
+          // noFly={true}
+          verticalMax={Math.PI / 2}
+          // vericalMin={Math.PI / 2}
+          // lookVertical={true}
+        />
+      ) : (
+        <></>
+      )}
+      {oPControl ? (
+        <orbitControls
+          ref={controlsRef}
+          args={[camera, domElement]}
+          enableZoom={true}
+          maxZoom={100}
+          minZoom={10}
+          enabled={oPControl}
+          // enablePan={true}
+          maxDistance={1000}
+          minPolarAngle={0}
+          maxPolarAngle={Math.PI / 2}
+          zoomSpeed={0.5}
+          enableDamping={true}
+          screenSpacePanning={true}
+          keyPanSpeed={40}
+        />
+      ) : (
+        <></>
+      )}
     </>
   );
 }
